8ab818eca06c90252e02d97ef07183041258609b
Aleksandr <aleksandr.tarejev@techin.lt>
2026-02-23 19:45:33 +0200

    job with axios and Effect

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "react-bootstrap/Card";
+import "./ProductList.css";
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://dummyjson.com/products?limit=50").then((response) => {
+      setProducts(response.data.products);
+    });
+  }, []);
+  if (!products) {
+    return <p>Loading....</p>;
+  }
+
+  return (
+    <div className="container">
+      {products.map((product) => (
+        <Card style={{ width: "18rem" }} key={product.id}>
+          {/* <Card.Img variant="top" src={product.images} /> */}
+          <Card.Body>
+            <Card.Title>{product.title}</Card.Title>
+            <Card.Text>{product.description}</Card.Text>
+            <Card.Text>Price: {product.price} eur</Card.Text>
+            <Card.Text>Rating: {product.rating}</Card.Text>
+            <Card.Text
+              className={product.stock ? "text-success" : "text-danger"}
+            >
+              Stock: {product.stock}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default ProductList;
